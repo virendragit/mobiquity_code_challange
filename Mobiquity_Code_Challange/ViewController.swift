afc3e7e2386d944d5480f152f6a1a5a22cf2fd95
Virendra Gupta <>
2021-07-31 18:54:17 +0530

    added prject files

---
+++ Mobiquity_Code_Challange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobiquity_Code_Challange
+//
+//  Created by Virendra Gupta on 31/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
