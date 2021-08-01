b11fcdbcece7f1c0143476688e0a28326f482198
Virendra Gupta <>
2021-08-01 11:57:05 +0530

    added common file for for configuration

---
+++ Mobiquity_Code_Challange/Common/Common.swift
@@ -0,0 +1,23 @@
+//
+//  Common.swift
+//  Mobiquity_Code_Challange
+//
+//  Created by Virendra Gupta on 01/08/21.
+//
+
+import Foundation
+
+struct Constants {
+    static let ErrorAlertTitle = "Error"
+    static let OkAlertTitle = "Ok"
+    static let CancelAlertTitle = "Cancel"
+    
+    static let apiKey = "fae7190d7e6433ec3a45285ffcf55c86"
+
+}
+
+struct ApiEndpoints
+{
+    static let todayForcast = "http:// api.openweathermap.org/data/2.5/weather?q=london&appid=\(Constants.apiKey))"
+    static let fiveDayForcast = "http://api.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=\(Constants.apiKey)&units=metric"
+}
